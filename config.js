6d48ef97391178fcc1a3f395950f8b13bbd8c062
Andrew Sutherland <azsuth@gmail.com>
2019-04-16 11:25:24 -0500

    initial commit for handling auth on /api endpoints

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.PORT || 3001,
+    clientId: process.env.CLIENT_ID || '54851119520-ioolc277euiajnnqt3vdo2r9r8sqse9g.apps.googleusercontent.com'
+};
--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello, world!");
+const express = require('express');
+const proxy = require('express-http-proxy');
+const { OAuth2Client } = require('google-auth-library');
+const { port, clientId } = require('./config');
+
+const app = express();
+const client = new OAuth2Client(clientId);
+
+async function verify(idToken) {
+    const ticket = await client.verifyIdToken({
+        idToken: idToken,
+        audience: clientId
+    });
+
+    const payload = ticket.getPayload();
+    const userId = payload['sub'];
+
+    console.log('User ID: ', userId);
+}
+
+app.use((req, res, next) => {
+    if (!req.path.startsWith('/api')) {
+        next();
+        return;
+    }
+
+    console.log('Beginning auth...');
+
+    const idToken = req.header('idToken');
+    verify(idToken)
+        .then(next)
+        .catch((err) => {
+            console.error(err);
+            res.send('Error');
+        });
+});
+
+app.use('/', proxy('http://router'));
+
+app.listen(port, () => {
+    console.log("Gateway listening on port " + port);
+});
