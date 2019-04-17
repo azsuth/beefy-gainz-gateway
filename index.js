5d8c8a671130935e0414e5460c5df22bd4237a65
Andrew Sutherland <azsuth@gmail.com>
2019-04-17 10:37:02 -0500

    hardened the service, added nodemon for local development

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { port, clientId } = require('./config');
 const app = express();
 const client = new OAuth2Client(clientId);
 
+// throws an error if the idToken from Google is invalid
 async function verify(idToken) {
     const ticket = await client.verifyIdToken({
         idToken: idToken,
@@ -15,28 +16,35 @@ async function verify(idToken) {
     const payload = ticket.getPayload();
     const userId = payload['sub'];
 
-    console.log('User ID: ', userId);
+    return userId;
 }
 
+// catch all to handle authentication
 app.use((req, res, next) => {
+    // only authenticate requests to paths starting with /api
     if (!req.path.startsWith('/api')) {
         next();
         return;
     }
 
-    console.log('Beginning auth...');
+    console.log('asdf andrew sutherland');
 
     const idToken = req.header('idToken');
     verify(idToken)
-        .then(next)
+        .then((userId) => {
+            // pass the userId to all upstream services
+            req.headers['userId'] = userId;
+            next();
+        })
         .catch((err) => {
             console.error(err);
-            res.send('Error');
+            res.status(401).send();
         });
 });
 
-app.use('/', proxy('http://router'));
+// catch all to proxy all requests to the router
+app.use(proxy('http://router'));
 
 app.listen(port, () => {
-    console.log("Gateway listening on port " + port);
+    console.log('Gateway listening on port ' + port);
 });
