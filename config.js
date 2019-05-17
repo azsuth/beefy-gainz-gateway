0cdbeac54e283a5342530d0deee4f325367707b2
Andrew Sutherland <azsuth@gmail.com>
2019-05-17 10:44:08 -0500

    renamed environment variables to be more specific

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    port: process.env.PORT || 3001,
-    clientId: process.env.CLIENT_ID || '54851119520-ioolc277euiajnnqt3vdo2r9r8sqse9g.apps.googleusercontent.com',
-    router: process.env.ROUTER || 'http://router',
-    userIdOverride: process.env.USER_ID_OVERRIDE
+    port: process.env.GATEWAY_PORT || 3001,
+    clientId: process.env.GATEWAY_CLIENT_ID || '54851119520-ioolc277euiajnnqt3vdo2r9r8sqse9g.apps.googleusercontent.com',
+    router: process.env.GATEWAY_ROUTER || 'http://router',
+    userIdOverride: process.env.GATEWAY_USER_ID_OVERRIDE
 };
