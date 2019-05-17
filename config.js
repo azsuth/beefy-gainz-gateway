a431bd32424ddebad001b6f052bc69449d45be1b
Andrew Sutherland <azsuth@gmail.com>
2019-05-17 12:29:03 -0500

    removed dependency on router service

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     port: process.env.GATEWAY_PORT || 3001,
     clientId: process.env.GATEWAY_CLIENT_ID || '54851119520-ioolc277euiajnnqt3vdo2r9r8sqse9g.apps.googleusercontent.com',
-    router: process.env.GATEWAY_ROUTER || 'http://router',
-    userIdOverride: process.env.GATEWAY_USER_ID_OVERRIDE
+    userIdOverride: process.env.GATEWAY_USER_ID_OVERRIDE,
+    clientUpstream: process.env.GATEWAY_CLIENT_UPSTREAM || 'http://client:3000',
+    exerciseUpstream: process.env.GATEWAY_EXERCISE_UPSTREAM || 'http://exercise:8080'
 };
--- index.js
+++ index.js
@@ -1,51 +1,67 @@
 const express = require('express');
 const proxy = require('express-http-proxy');
 const { OAuth2Client } = require('google-auth-library');
-const { port, clientId, router, userIdOverride } = require('./config');
+
+const {
+  port,
+  clientId,
+  userIdOverride,
+  clientUpstream,
+  exerciseUpstream
+} = require('./config');
 
 const app = express();
 const client = new OAuth2Client(clientId);
 
 // throws an error if the idToken from Google is invalid
 async function verify(idToken) {
-    const ticket = await client.verifyIdToken({
-        idToken: idToken,
-        audience: clientId
-    });
+  const ticket = await client.verifyIdToken({
+    idToken: idToken,
+    audience: clientId
+  });
 
-    const payload = ticket.getPayload();
-    const userId = payload['sub'];
+  const payload = ticket.getPayload();
+  const userId = payload['sub'];
 
-    console.log('idToken:', idToken);
-    console.log('userId:', userId);
+  console.log('idToken:', idToken);
+  console.log('userId:', userId);
 
-    return userId;
+  return userId;
 }
 
 // catch all to handle authentication
 app.use((req, res, next) => {
-    // only authenticate requests to paths starting with /api
-    if (!req.path.startsWith('/api')) {
-        next();
-        return;
-    }
+  // only authenticate requests to paths starting with /api
+  if (!req.path.startsWith('/api')) {
+    next();
+    return;
+  }
 
-    const idToken = req.header('idToken');
-    verify(idToken)
-        .then((userId) => {
-            // pass the userId to all upstream services
-            req.headers['userId'] = userIdOverride || userId;
-            next();
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(401).send();
-        });
+  const idToken = req.header('idToken');
+  verify(idToken)
+    .then(userId => {
+      // pass the userId to all upstream services
+      req.headers['userId'] = userIdOverride || userId;
+      next();
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(401).send();
+    });
 });
 
-// catch all to proxy all requests to the router
-app.use(proxy(router));
+app.use(
+  '/api/exercises',
+  proxy(exerciseUpstream, {
+    proxyReqPathResolver: req => {
+      const query = req.url.split('?');
+      return `/exercises?${query[1]}`;
+    }
+  })
+);
+
+app.use('/', proxy(clientUpstream));
 
 app.listen(port, () => {
-    console.log('Gateway listening on port ' + port);
+  console.log('Gateway listening on port ' + port);
 });
