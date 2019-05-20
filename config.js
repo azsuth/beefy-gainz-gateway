83c711f1181f3028f5a963e79d7c104cb217a8a9
Andrew Sutherland <azsuth@gmail.com>
2019-05-20 11:21:44 -0500

    removed google client ID from config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.GATEWAY_PORT || 3001,
-    clientId: process.env.GATEWAY_CLIENT_ID || '54851119520-ioolc277euiajnnqt3vdo2r9r8sqse9g.apps.googleusercontent.com',
+    clientId: process.env.GATEWAY_GOOGLE_CLIENT_ID,
     userIdOverride: process.env.GATEWAY_USER_ID_OVERRIDE,
     exerciseUpstream: process.env.GATEWAY_EXERCISE_UPSTREAM || 'http://exercise:8080'
 };
